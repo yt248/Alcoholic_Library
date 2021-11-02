0a2e616e433531be0d3938dd4464cd705d0b56de
yt248 <yauhen.tsaryk@gmai.com>
2021-11-02 22:35:58 +0300

    Project Initializr

---
+++ src/main/java/com/eugene/Alcoholic_Library/AlcoholicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.eugene.Alcoholic_Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eugene/Alcoholic_Library/AlcoholicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugene.Alcoholic_Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
